1a98d241bde70f4ffc324b708380dac28c9a5174
Miranda <miranda@Mirandas-MacBook-Air-2.local>
2025-12-01 23:03:19 -0500

    third main sketch added to inter portal sketch

--- js/p5-pantograph/scripts/inter_portal_sketch.js
+++ js/p5-pantograph/scripts/inter_portal_sketch.js
@@ -5,7 +5,7 @@ let sketch1 = new p5( p => {
     p.setup = () => {
       // canvas size is specified in the CSS file (size of div #one)
       const one= document.getElementById('src1');
-      p.createCanvas(one.clientWidth, one.clientHeight);
+      p.createCanvas(300,200);
       // same as: p.createCanvas($("#one").width(), $("#one").height());
     };
   
@@ -26,7 +26,7 @@ let sketch2 = new p5( p => {
   p.setup = () => {
     // canvas size is specified in the CSS file (size of div #two)
     const two= document.getElementById('src2');
-    p.createCanvas(two.clientWidth, two.clientHeight);
+    p.createCanvas(300, 200);
     // same as: p.createCanvas($("#two").width(), $("#two").height());
   };
 
@@ -37,3 +37,19 @@ let sketch2 = new p5( p => {
     p.ellipse(50, 50, 50, 50);
   };
 }, 'src2');
+
+let mainSketch = new p5( p => {
+
+  p.setup = () => {
+    // canvas size is specified in the CSS file (size of div #two)
+    const canvasContainer = document.getElementById('canvas-container');
+    p.createCanvas(600,400);
+  };
+
+  p.draw = () => {
+    p.background(170);
+    p.noStroke();
+    p.fill(255);
+    p.ellipse(50, 50, 50, 50);
+  };
+}, 'canvas-container');
