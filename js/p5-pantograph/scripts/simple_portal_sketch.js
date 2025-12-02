84cce09ba3ef3994c18e5f86f462c01867aa3955
Miranda <miranda@Mirandas-MacBook-Air-2.local>
2025-12-01 21:43:16 -0500

    stroke follows circle movement, + figured out fast blur

--- js/p5-pantograph/scripts/simple_portal_sketch.js
+++ js/p5-pantograph/scripts/simple_portal_sketch.js
@@ -1,5 +1,8 @@
 import {sketch} from 'p5js-wrapper';
 
+// Array to store the path positions
+let path = [];
+
 sketch.setup = function(){
   const canvas = createCanvas(800, 600);
   canvas.parent('canvas-container');
@@ -7,8 +10,6 @@ sketch.setup = function(){
 
 sketch.draw= function(){
   background(255);
-  fill(0,0,0);
-  noStroke();
   
   // Get position from simple_portal.js
   const posX = window.portalPosition?.x || 0;
@@ -27,5 +28,24 @@ sketch.draw= function(){
   const canvasX = map(posX, -100, 100, 0, width);
   const canvasY = map(adjustedPosY, -100, 100, 0, height);
   
+  // Add current position to path
+  path.push({x: canvasX, y: canvasY});
+  
+  // Draw the path (stroke)
+  if (path.length > 1) {
+    stroke(16, 61, 70);
+    strokeWeight(2);
+    noFill();
+    beginShape();
+    for (let i = 0; i < path.length; i++) {
+      vertex(path[i].x, path[i].y);
+    }
+    endShape();
+  }
+  
+  // Draw the circle at current position
+  fill(16, 61, 70);
+  noStroke();
   circle(canvasX, canvasY, 50);
+  drawingContext.filter = 'blur(6px)';
 }
